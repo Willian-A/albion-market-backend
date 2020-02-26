3992ceb257978bf92b5d587411f1305f4af8a6c4
Willian Almeida <36938744+Willian-A@users.noreply.github.com>
2020-02-26 01:43:43 -0300

    Correção de bugs
    remoção do bug de mais de 6 ciadades/
    itemEnchant para itemQuality/
    melhoria do filtro para qualidade tbm/
    retirada temporaria do for em httpRequest

--- src/controllers/SeachItensController.js
+++ src/controllers/SeachItensController.js
@@ -4,19 +4,19 @@ const filter = require("./SearchFilterController");
 // Controller normalmente tem index, show, store, update, destroy
 
 module.exports = {
-  async index(request, response) {
-    var { itemName, itemEnchant } = request.body;
+    async index(request, response) {
+        var { itemName, itemQuality } = request.body;
 
-    itemName = await filter(itemName.trim());
+        itemName = await filter(itemName.trim(), itemQuality.trim());
 
-    if (itemName == false) {
-      return response.json({ search: false });
+        if (itemName == false) {
+            return response.json({ search: false });
+        }
+        console.log(
+            `Came As --> ${request.body.itemName.trim()} // Goes As --> ${itemName}`
+        );
+
+        var search = await httpGet(itemName, itemQuality);
+        return response.json({ search });
     }
-    console.log(
-      `Came As --> ${request.body.itemName.trim()} // Goes As --> ${itemName}`
-    );
-
-    var search = await httpGet(itemName, itemEnchant);
-    return response.json({ search });
-  }
 };
--- src/controllers/SearchFilterController.js
+++ src/controllers/SearchFilterController.js
@@ -1,18 +1,23 @@
 const data = require("./list.json");
 
-module.exports = function filter(itemName) {
-  var tData = JSON.parse(JSON.stringify(data));
-  var ok = false;
+module.exports = function filter(itemName, itemQuality) {
+    var tData = JSON.parse(JSON.stringify(data));
+    var ok = false;
 
-  for (var i = 0; i <= 6705; ++i in tData) {
-    if (itemName == tData[i].LocalizedNames["PT-BR"]) {
-      itemName = tData[i].UniqueName;
-      ok = true;
+    for (var i = 0; i <= 6705; ++i in tData) {
+        if (itemName == tData[i].LocalizedNames["PT-BR"]) {
+            itemName = tData[i].UniqueName;
+            ok = true;
+        }
     }
-  }
 
-  if (ok === false) {
-    return false;
-  }
-  return itemName;
+    if (ok === false) {
+        return false;
+    }
+    console.log(itemQuality);
+    if (itemQuality >= 6) {
+        return false;
+    }
+
+    return itemName;
 };
