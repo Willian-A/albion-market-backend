0786302427bda9308f6ae430cad36727faa984c9
Willian Almeida <36938744+Willian-A@users.noreply.github.com>
2020-04-14 05:13:30 -0300

    Update SeachItensController.js

--- src/controllers/SeachItensController.js
+++ src/controllers/SeachItensController.js
@@ -4,19 +4,19 @@ const filter = require("./SearchFilterController");
 // Controller normalmente tem index, show, store, update, destroy
 
 module.exports = {
-    async index(request, response) {
-        var { itemName, itemQuality } = request.body;
+  async index(request, response) {
+    var { itemName, itemQuality } = request.body;
 
-        itemName = await filter(itemName.trim(), itemQuality.trim());
+    itemName = await filter(itemName.trim(), itemQuality.trim());
 
-        if (itemName == false) {
-            return response.json({ search: false });
-        } else {
-            console.log(
-                `Came As --> ${request.body.itemName.trim()} // Goes As --> ${itemName} With ${itemQuality} as Quality`
-            );
-            var search = await httpGet(itemName, itemQuality);
-            return response.json({ search });
-        }
+    if (itemName == false) {
+      return response.json({ search: false });
+    } else {
+      console.log(
+        `Came As --> ${request.body.itemName.trim()} // Goes As --> ${itemName} With ${itemQuality} as Quality`
+      );
+      var search = await httpGet(itemName, itemQuality);
+      return response.json({ search });
     }
+  }
 };
